7ea555664faad14a6cc516daf5a58b027480d80f
Luhsey <123013240+Luhsey@users.noreply.github.com>
2023-04-01 10:27:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,153 @@
-const calculate = (initialNumber) => {
-  let currentResult = initialNumber;
+class MockServer {
+  #users = [];
 
-  const add = (num) => {
-    currentResult += num;
-    return currentResult;
-  };
+  static getAllUsers() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.#users);
+      }, 1000);
+    });
+  }
 
-  const subtract = (num) => {
-    currentResult -= num;
-    return currentResult;
-  };
+  static getUser(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const user = this.#users.find((user) => user.id === id);
+        if (user) {
+          resolve(user);
+        } else {
+          reject(new Error(`User with id ${id} not found`));
+        }
+      }, 1000);
+    });
+  }
 
-  const multiply = (num) => {
-    currentResult *= num;
-    return currentResult;
-  };
+  static addUser(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user && user.id && user.name && user.age) {
+          this.#users.push(user);
+          resolve(true);
+        } else {
+          reject(new Error(`User object is not valid`));
+        }
+      }, 1000);
+    });
+  }
 
-  const divide = (num) => {
-    currentResult /= num;
-    return currentResult;
-  };
+  static updateUser(id, user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const index = this.#users.findIndex((user) => user.id === id);
+        if (index !== -1) {
+          this.#users[index] = { ...this.#users[index], ...user };
+          resolve(this.#users);
+        } else {
+          reject(new Error(`User with id ${id} not found`));
+        }
+      }, 1000);
+    });
+  }
 
-  const reset = () => {
-    currentResult = initialNumber;
-    return currentResult;
-  };
+  static deleteUser(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const index = this.#users.findIndex((user) => user.id === id);
+        if (index !== -1) {
+          this.#users.splice(index, 1);
+          resolve(true);
+        } else {
+          reject(new Error(`User with id ${id} not found`));
+        }
+      }, 1000);
+    });
+  }
+}
+MockServer.addUser({ id: 1, name: "John", age: 30 })
+  .then(() => MockServer.getAllUsers())
+  .then((users) => console.log(users))
+  .catch((err) => console.log(err));
 
-  return { add, subtract, multiply, divide, reset };
-};
-const calculator = calculate(5);
-console.log(calculator.add(5));
-console.log(calculator.subtract(2));
-console.log(calculator.multiply(3));
-console.log(calculator.divide(4));
-console.log(calculator.reset());
-/////////////////////////////////////////////////////////
-const cacheDecorator = (func) => {
-  const cache = {};
+MockServer.getUser(1)
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
 
-  return function (...args) {
-    const key = JSON.stringify(args);
+MockServer.updateUser(1, { age: 31 })
+  .then((users) => console.log(users))
+  .catch((err) => console.log(err));
 
-    if (key in cache) {
-      console.log("Returning result from cache for key:", key);
-      return cache[key];
-    }
+MockServer.deleteUser(1)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
 
-    const result = func.apply(this, args);
-    cache[key] = result;
-    console.log("Caching result for key:", key);
-
-    return result;
-  };
-};
-const sum = (num) => {
-  return num + num;
-};
-
-const decoratedSum = cacheDecorator(sum);
-console.log(decoratedSum(2));
-console.log(decoratedSum(2));
-
-const obj = {
-  num: 1,
-  result: null,
-  sum(num) {
-    return this.num + num;
-  },
-};
-
-const decoratedObjSum = cacheDecorator(obj.sum.bind(obj));
-console.log(decoratedObjSum(3));
-console.log(decoratedObjSum(3));
-////////////////////////////////////////////////////////
-const factorial = (initialNumber) => {
-  if (initialNumber === 0) {
-    return 1;
-  } else {
-    return initialNumber * factorial(initialNumber - 1);
+const createUsers = async (usersArray) => {
+  for (const user of usersArray) {
+    await MockServer.addUser(user);
   }
 };
 
-console.log(factorial(5));
+const users = [
+  { id: 2, name: "Jane", age: 25 },
+  { id: 3, name: "Bob", age: 40 },
+  { id: 4, name: "Mary", age: 35 },
+];
+
+createUsers(users).catch((err) => console.log(err));
+const readAllUsers = async () => {
+  try {
+    const users = await MockServer.getAllUsers();
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readUserById = async (id) => {
+  try {
+  const user = await MockServer.getUser(id);
+  console.log(user);
+  } catch (err) {
+  console.log(err);
+  }
+  };
+  
+  const addUser = async (user) => {
+  try {
+  const result = await MockServer.addUser(user);
+  console.log(result);
+  } catch (err) {
+  console.log(err);
+  }
+  };
+  
+  const updateUser = async (id, user) => {
+  try {
+  const result = await MockServer.updateUser(id, user);
+  console.log(result);
+  } catch (err) {
+  console.log(err);
+  }
+  };
+  
+  const deleteUser = async (id) => {
+  try {
+  const result = await MockServer.deleteUser(id);
+  console.log(result);
+  } catch (err) {
+  console.log(err);
+  }
+  };
+  
+  const createUsers = async () => {
+  const promises = [];
+  for (let i = 1; i <= 100; i++) {
+  const user = { id: i, name: User ${i}, age: Math.floor(Math.random() * 50) + 18 };
+  promises.push(MockServer.addUser(user));
+  }
+  try {
+  const results = await Promise.allSettled(promises);
+  console.log(results);
+  } catch (err) {
+  console.log(err);
+  }
+  };
