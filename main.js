d0366cb599b24fd617941aa5409ad54550c4c352
Luhsey <123013240+Luhsey@users.noreply.github.com>
2023-04-01 10:31:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,153 +1,102 @@
-class MockServer {
-  #users = [];
-
-  static getAllUsers() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.#users);
-      }, 1000);
-    });
+class UserService {
+  static async getAllUsers() {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!response.ok) {
+        throw new Error("Unable to fetch users");
+      }
+      const users = await response.json();
+      console.log(users);
+      return users;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
-  static getUser(id) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const user = this.#users.find((user) => user.id === id);
-        if (user) {
-          resolve(user);
-        } else {
-          reject(new Error(`User with id ${id} not found`));
-        }
-      }, 1000);
-    });
+  static async getUser(id) {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+      if (!response.ok) {
+        throw new Error(`User with id ${id} not found`);
+      }
+      const user = await response.json();
+      console.log(user);
+      return user;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
-  static addUser(user) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (user && user.id && user.name && user.age) {
-          this.#users.push(user);
-          resolve(true);
-        } else {
-          reject(new Error(`User object is not valid`));
-        }
-      }, 1000);
-    });
+  static async addUser(user) {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        throw new Error("Unable to add user");
+      }
+      const result = await response.json();
+      console.log(result);
+      return true;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
-  static updateUser(id, user) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const index = this.#users.findIndex((user) => user.id === id);
-        if (index !== -1) {
-          this.#users[index] = { ...this.#users[index], ...user };
-          resolve(this.#users);
-        } else {
-          reject(new Error(`User with id ${id} not found`));
-        }
-      }, 1000);
-    });
+  static async updateUser(id, user) {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        throw new Error(`User with id ${id} not found`);
+      }
+      const updatedUser = await response.json();
+      console.log(updatedUser);
+      return updatedUser;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
-  static deleteUser(id) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const index = this.#users.findIndex((user) => user.id === id);
-        if (index !== -1) {
-          this.#users.splice(index, 1);
-          resolve(true);
-        } else {
-          reject(new Error(`User with id ${id} not found`));
-        }
-      }, 1000);
-    });
+  static async deleteUser(id) {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`User with id ${id} not found`);
+      }
+      const result = await response.json();
+      console.log(result);
+      return true;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  static async deleteUsers() {
+    const idsToDelete = [1, 2, 3, 4, Math.floor(Math.random() * 9996) + 5];
+    const deleteRequests = idsToDelete.map(id => UserService.deleteUser(id));
+    try {
+      const results = await Promise.allSettled(deleteRequests);
+      console.log(results);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 }
-MockServer.addUser({ id: 1, name: "John", age: 30 })
-  .then(() => MockServer.getAllUsers())
-  .then((users) => console.log(users))
-  .catch((err) => console.log(err));
-
-MockServer.getUser(1)
-  .then((user) => console.log(user))
-  .catch((err) => console.log(err));
-
-MockServer.updateUser(1, { age: 31 })
-  .then((users) => console.log(users))
-  .catch((err) => console.log(err));
-
-MockServer.deleteUser(1)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
-
-const createUsers = async (usersArray) => {
-  for (const user of usersArray) {
-    await MockServer.addUser(user);
-  }
-};
-
-const users = [
-  { id: 2, name: "Jane", age: 25 },
-  { id: 3, name: "Bob", age: 40 },
-  { id: 4, name: "Mary", age: 35 },
-];
-
-createUsers(users).catch((err) => console.log(err));
-const readAllUsers = async () => {
-  try {
-    const users = await MockServer.getAllUsers();
-    console.log(users);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const readUserById = async (id) => {
-  try {
-  const user = await MockServer.getUser(id);
-  console.log(user);
-  } catch (err) {
-  console.log(err);
-  }
-  };
-  
-  const addUser = async (user) => {
-  try {
-  const result = await MockServer.addUser(user);
-  console.log(result);
-  } catch (err) {
-  console.log(err);
-  }
-  };
-  
-  const updateUser = async (id, user) => {
-  try {
-  const result = await MockServer.updateUser(id, user);
-  console.log(result);
-  } catch (err) {
-  console.log(err);
-  }
-  };
-  
-  const deleteUser = async (id) => {
-  try {
-  const result = await MockServer.deleteUser(id);
-  console.log(result);
-  } catch (err) {
-  console.log(err);
-  }
-  };
-  
-  const createUsers = async () => {
-  const promises = [];
-  for (let i = 1; i <= 100; i++) {
-  const user = { id: i, name: User ${i}, age: Math.floor(Math.random() * 50) + 18 };
-  promises.push(MockServer.addUser(user));
-  }
-  try {
-  const results = await Promise.allSettled(promises);
-  console.log(results);
-  } catch (err) {
-  console.log(err);
-  }
-  };
