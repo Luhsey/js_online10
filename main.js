ae3a55da64e0f5a35d5ef93033ba6a5815b3c817
Luhsey <123013240+Luhsey@users.noreply.github.com>
2023-04-01 10:37:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,102 +1,57 @@
-class UserService {
-  static async getAllUsers() {
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      if (!response.ok) {
-        throw new Error("Unable to fetch users");
-      }
-      const users = await response.json();
-      console.log(users);
-      return users;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
+import { Octokit } from "https://cdn.skypack.dev/octokit";
 
-  static async getUser(id) {
-    try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-      if (!response.ok) {
-        throw new Error(`User with id ${id} not found`);
-      }
-      const user = await response.json();
-      console.log(user);
-      return user;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
+const octokit = new Octokit({
+  auth: "YOUR_KEY",
+});
 
-  static async addUser(user) {
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const users = await octokit.rest.users.list();
+    const usersContainer = document.getElementById("users-container");
+    const organizationsContainer = document.getElementById(
+      "organizations-container"
+    );
+
+    usersContainer.innerHTML = "";
+    organizationsContainer.innerHTML = "";
+
+    for (const user of users.data) {
+      const userDiv = document.createElement("div");
+      userDiv.classList.add("user");
+
+      const avatar = document.createElement("img");
+      avatar.src = user.avatar_url;
+      userDiv.appendChild(avatar);
+
+      const name = document.createElement("div");
+      name.textContent = user.name;
+      userDiv.appendChild(name);
+
+      const nickname = document.createElement("div");
+      nickname.textContent = user.login;
+      userDiv.appendChild(nickname);
+
+      const reposCount = document.createElement("div");
+      reposCount.textContent = `Repos: ${user.public_repos}`;
+      userDiv.appendChild(reposCount);
+
+      const followersCount = document.createElement("div");
+      followersCount.textContent = `Followers: ${user.followers}`;
+      userDiv.appendChild(followersCount);
+
+      usersContainer.appendChild(userDiv);
+
+      const orgs = await octokit.rest.users.listOrgsForUser({
+        username: user.login,
       });
-      if (!response.ok) {
-        throw new Error("Unable to add user");
-      }
-      const result = await response.json();
-      console.log(result);
-      return true;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
 
-  static async updateUser(id, user) {
-    try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      if (!response.ok) {
-        throw new Error(`User with id ${id} not found`);
+      for (const org of orgs.data) {
+        const orgImg = document.createElement("img");
+        orgImg.src = org.avatar_url;
+        organizationsContainer.appendChild(orgImg);
       }
-      const updatedUser = await response.json();
-      console.log(updatedUser);
-      return updatedUser;
-    } catch (err) {
-      console.error(err);
-      throw err;
     }
+  } catch (error) {
+    console.error(error);
   }
-
-  static async deleteUser(id) {
-    try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-        method: "DELETE",
-      });
-      if (!response.ok) {
-        throw new Error(`User with id ${id} not found`);
-      }
-      const result = await response.json();
-      console.log(result);
-      return true;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
-
-  static async deleteUsers() {
-    const idsToDelete = [1, 2, 3, 4, Math.floor(Math.random() * 9996) + 5];
-    const deleteRequests = idsToDelete.map(id => UserService.deleteUser(id));
-    try {
-      const results = await Promise.allSettled(deleteRequests);
-      console.log(results);
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
-}
+});
