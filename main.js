3c6fe0f546e1d0215590e086ee9053273de6deac
Luhsey <123013240+Luhsey@users.noreply.github.com>
2023-04-01 10:15:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,97 +1,146 @@
-function getDaysInYear(year) {
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return 366;
-  } else {
-    return 365;
-  }
-}
-console.log(getDaysInYear(2021)); 
-console.log(getDaysInYear(2020));
-//////////////////////////////////////////////////
-function getDayNumber(dateString) {
-  const date = new Date(dateString);
-  const startOfYear = new Date(date.getFullYear(), 0, 1);
-  const diff = date.getTime() - startOfYear.getTime();
-  const oneDay = 1000 * 60 * 60 * 24;
-  return Math.ceil(diff / oneDay);
-}
-console.log(getDayNumber("2023-01-12"));
-console.log(getDayNumber("2023-02-26"));
-/////////////////////////////////////////////////
-function getQuarters(dateString) {
-  const date = new Date(dateString);
-  const month = date.getMonth() + 1;
-  if (month >= 1 && month <= 3) {
-    return "I quarter";
-  } else if (month >= 4 && month <= 6) {
-    return "II quarter";
-  } else if (month >= 7 && month <= 9) {
-    return "III quarter";
-  } else {
-    return "IV quarter";
-  }
-}
-console.log(getQuarters("2023-02-26"));
-/////////////////////////////////////////////////
-function calcDateDiff(startDate, endDate) {
-  const start = new Date(startDate);
-  const end = new Date(endDate);
-  const diff = end - start;
-  if (diff < 60 * 1000) {
-    return `${Math.floor(diff / 1000)} seconds`;
-  } else if (diff < 60 * 60 * 1000) {
-    const minutes = Math.floor(diff / (60 * 1000));
-    return `${minutes} ${minutes === 1 ? "minute" : "minutes"}`;
-  } else if (diff < 24 * 60 * 60 * 1000) {
-    const hours = Math.floor(diff / (60 * 60 * 1000));
-    const minutes = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
-    return `${hours} ${hours === 1 ? "hour" : "hours"} ${minutes} ${minutes === 1 ? "minute" : "minutes"}`;
-  } else if (diff < 30 * 24 * 60 * 60 * 1000) {
-    const days = Math.floor(diff / (24 * 60 * 60 * 1000));
-    return `${days} ${days === 1 ? "day" : "days"}`;
-  } else if (diff < 365 * 24 * 60 * 60 * 1000) {
-    const months = Math.floor(diff / (30 * 24 * 60 * 60 * 1000));
-    return `${months} ${months === 1 ? "month" : "months"}`;
-  } else {
-    const years = Math.floor(diff / (365 * 24 * 60 * 60 * 1000));
-    return `${years} ${years === 1 ? "year" : "years"}`;
-  }
-}
-console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 14:20"));
-console.log(calcDateDiff("2023-02-26 14"))
-/////////////////////////////////////////////////////
-function createMap(arr) {
+const unique = (arr) => {
   const map = new Map();
-  arr.forEach(obj => {
-    map.set(obj.id, obj);
-  });
-  return map;
-}
-const arr = [
-  { id: 1, value: 1, date: "2022-02-15" },
-  { id: 2, value: 1242, date: "2023-02-15" },
-  { id: 3, value: 3342, date: "2021-02-15" },
-];
-const map = createMap(arr);
-console.log(map.get(1));
-console.log(map.get(2));
-console.log(map.get(3)); 
-////////////////////////////////////////////////////
-function cacheCalcDateDiff() {
-  const cache = new Map();
-  return function(startDate, endDate) {
-    const key = `${startDate}_${endDate}`;
-    if (cache.has(key)) {
-      return cache.get(key);
-    } else {
-      const diff = calcDateDiff(startDate, endDate);
-      cache.set(key, diff);
-      return diff;
+  for (const word of arr) {
+    const sorted = word.split('').sort().join('');
+    if (!map.has(sorted)) {
+      map.set(sorted, word);
     }
   }
+  return Array.from(map.values());
+};
+const anagrams = [
+  "actor",
+  "carot",
+  "listen",
+  "enlist",
+  "debit",
+  "bidet",
+  "tear",
+  "rate",
+  "night",
+  "thing",
+  "lives",
+  "veils",
+  "stressed",
+  "desserts",
+  "dormitory",
+  "dirty room",
+  "rescue",
+  "secure",
+  "a gentleman",
+  "elegant man",
+  "listen",
+  "heart",
+  "angel",
+  "leader",
+];
+console.log(unique(anagrams));
+//////////////////////////////////////
+const user = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  get userInfo() {
+    return `${this.firstName} ${this.lastName} is ${this.age}`;
+  },
+  set userInfo(info) {
+    if (typeof info === "string") {
+      const [firstName, lastName, age] = info.split(" ");
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    } else if (typeof info === "object") {
+      const { firstName, lastName, age } = info;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+  },
+};
+user.userInfo = "Taras Samoilenko 25";
+console.log(user.userInfo);
+user.userInfo = { firstName: "Kate", lastName: "Karp", age: 22 };
+console.log(user.userInfo); 
+/////////////////////////////////////////////
+const obj = {
+  from: 1,
+  to: 10,
+  createArr() {
+    this.arr = [];
+    for (let i = this.from; i <= this.to; i++) {
+      this.arr.push(i);
+    }
+    return this.arr;
+  },
+};
+obj.createArr();
+console.log(obj.arr);
+///////////////////////////////////////////////
+function Car(model, color, age, speed, gasTank, started) {
+  this.model = model;
+  this.color = color;
+  this.age = age;
+  this.speed = speed;
+  this.gasTank = gasTank;
+  this.started = started;
+
+  this.startEngine = function() {
+    if (this.gasTank > 0) {
+      this.started = true;
+    }
+    return this;
+  };
+
+  this.drive = function() {
+    if (this.started && this.gasTank > 0) {
+      this.speed = 30;
+    }
+    return this;
+  };
+
+  this.stop = function() {
+    this.started = false;
+    this.speed = 0;
+    return this;
+  };
+
+  this.speedUp = function(amount) {
+    if (this.started && this.gasTank > 0) {
+      this.speed += amount;
+      if (this.speed > 200) {
+        this.speed = 200;
+      }
+      this.gasTank -= 5;
+      if (this.gasTank < 0) {
+        this.gasTank = 0;
+        this.stop();
+      }
+    }
+    return this;
+  };
+
+  this.slowDown = function(amount) {
+    if (this.started && this.gasTank > 0) {
+      this.speed -= amount;
+      if (this.speed < 0) {
+        this.speed = 0;
+      }
+      this.gasTank -= 5;
+      if (this.gasTank < 0) {
+        this.gasTank = 0;
+        this.stop();
+      }
+    }
+    return this;
+  };
+
+  this.addGas = function(amount) {
+    this.gasTank += amount;
+    if (this.gasTank > 20) {
+      this.gasTank = 20;
+    }
+    return this;
+  };
+
 }
-const cachedCalcDateDiff = cacheCalcDateDiff();
-const result1 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-26 14:20");
-const result2 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-26 14:20");
-const result3 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-27 15:30");
-const result4 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-27 15:30");
+const car = new Car('Toyota', 'red', 5, 0, 10, false);
