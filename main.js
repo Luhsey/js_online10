01160ae68394c348bd560a65184937ee977e4d33
Luhsey <123013240+Luhsey@users.noreply.github.com>
2023-04-01 10:12:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,97 @@
-function toTitleCase(str) {
-  const titleCase = str
-    .split(" ")
-    .map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-  return titleCase;
-}
-console.log(toTitleCase("my name is taras"));
-
-truncate = function (str, length, ending) {
-  if (length == null) {
-    length = 100;
-  }
-  if (ending == null) {
-    ending = "...";
-  }
-  if (str.length > length) {
-    return str.substring(0, length - ending.length) + ending;
+function getDaysInYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return 366;
   } else {
-    return str;
+    return 365;
   }
-};
-console.log(truncate("Lorem ipsum dolor sit amet, consectetur.", 14));
-console.log(truncate("Lorem ipsum dolor sit amet, consectetur.", 255));
-
-const filterRange = (arr, from, to) => {
-  return arr.filter((item) => item >= from && item <= to);
-};
-
-const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(filterRange(numArr, 3, 7));
-
-const sortArr = (arr) => arr.sort((a, b) => a - b);
-const arrToSort = [324, 32423, -3242, 544, 0, 23, -454, 22, 4];
-sortArr(arrToSort);
-console.log(sortArr(arrToSort));
-
-const sortStringArr = (arr) => arr.sort((a, b) => a.length - b.length);
-const stringArr = ["4534", "a", "bb", "sdfds", "", " ", "r4rdv-"];
-console.log(sortStringArr(stringArr));
-
-const usersArr = [
-  { name: "John", age: 25 },
-  { name: "John", age: 5 },
-  { name: "John", age: 2 },
-  { name: "John", age: 45 },
-  { name: "Pete", age: 30 },
-  { name: "Mary", age: 29 },
-  { name: "Mary", age: 2 },
-  { name: "Taras", age: 25 },
-  { name: "Taras", age: 19 },
-  { name: "Kate", age: 74 },
-  { name: "Chris", age: 14 },
-  { name: "Alan", age: 5 },
-  { name: "Alan", age: 32 },
-  { name: "Boris", age: 55 },
-  { name: "Elizabeth", age: 48 },
-  { name: "Elizabeth", age: 8 },
-];
-
-const sortAge = (arr) => {
-  const filteredArr = arr.filter((item) => item.age > 17 && item.age < 55);
-  const sum = filteredArr.reduce((acc, item) => (acc += item.age), 0);
-
-  return sum / filteredArr.length;
-};
-console.log(sortAge(usersArr));
-
-const sortUsers = (arr) => {
-  return arr.sort((a, b) => {
-    const textA = a.name.toUpperCase();
-    const textB = b.name.toUpperCase();
-    return textA < textB ? -1 : textA > textB ? 1 : a.age - b.age;
+}
+console.log(getDaysInYear(2021)); 
+console.log(getDaysInYear(2020));
+//////////////////////////////////////////////////
+function getDayNumber(dateString) {
+  const date = new Date(dateString);
+  const startOfYear = new Date(date.getFullYear(), 0, 1);
+  const diff = date.getTime() - startOfYear.getTime();
+  const oneDay = 1000 * 60 * 60 * 24;
+  return Math.ceil(diff / oneDay);
+}
+console.log(getDayNumber("2023-01-12"));
+console.log(getDayNumber("2023-02-26"));
+/////////////////////////////////////////////////
+function getQuarters(dateString) {
+  const date = new Date(dateString);
+  const month = date.getMonth() + 1;
+  if (month >= 1 && month <= 3) {
+    return "I quarter";
+  } else if (month >= 4 && month <= 6) {
+    return "II quarter";
+  } else if (month >= 7 && month <= 9) {
+    return "III quarter";
+  } else {
+    return "IV quarter";
+  }
+}
+console.log(getQuarters("2023-02-26"));
+/////////////////////////////////////////////////
+function calcDateDiff(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const diff = end - start;
+  if (diff < 60 * 1000) {
+    return `${Math.floor(diff / 1000)} seconds`;
+  } else if (diff < 60 * 60 * 1000) {
+    const minutes = Math.floor(diff / (60 * 1000));
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"}`;
+  } else if (diff < 24 * 60 * 60 * 1000) {
+    const hours = Math.floor(diff / (60 * 60 * 1000));
+    const minutes = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ${minutes} ${minutes === 1 ? "minute" : "minutes"}`;
+  } else if (diff < 30 * 24 * 60 * 60 * 1000) {
+    const days = Math.floor(diff / (24 * 60 * 60 * 1000));
+    return `${days} ${days === 1 ? "day" : "days"}`;
+  } else if (diff < 365 * 24 * 60 * 60 * 1000) {
+    const months = Math.floor(diff / (30 * 24 * 60 * 60 * 1000));
+    return `${months} ${months === 1 ? "month" : "months"}`;
+  } else {
+    const years = Math.floor(diff / (365 * 24 * 60 * 60 * 1000));
+    return `${years} ${years === 1 ? "year" : "years"}`;
+  }
+}
+console.log(calcDateDiff("2023-02-26 14:00", "2023-02-26 14:20"));
+console.log(calcDateDiff("2023-02-26 14"))
+/////////////////////////////////////////////////////
+function createMap(arr) {
+  const map = new Map();
+  arr.forEach(obj => {
+    map.set(obj.id, obj);
   });
-};
-console.log(sortUsers(usersArr));
-
-const strings = [
-  "Привіт",
-  "Світ",
-  "Привіт",
-  "Світ",
-  "Привіт",
-  "Привіт",
-  "Світ",
-  "Світ",
-  ":-O",
+  return map;
+}
+const arr = [
+  { id: 1, value: 1, date: "2022-02-15" },
+  { id: 2, value: 1242, date: "2023-02-15" },
+  { id: 3, value: 3342, date: "2021-02-15" },
 ];
-
-const unique = (arr) => {
-  const uniqueArr = [];
-  strings.forEach((str) => !u.includes(item) && u.push(item));
-
-  return uniqueArr;
-};
-
-console.log(unique(strings));
+const map = createMap(arr);
+console.log(map.get(1));
+console.log(map.get(2));
+console.log(map.get(3)); 
+////////////////////////////////////////////////////
+function cacheCalcDateDiff() {
+  const cache = new Map();
+  return function(startDate, endDate) {
+    const key = `${startDate}_${endDate}`;
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      const diff = calcDateDiff(startDate, endDate);
+      cache.set(key, diff);
+      return diff;
+    }
+  }
+}
+const cachedCalcDateDiff = cacheCalcDateDiff();
+const result1 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-26 14:20");
+const result2 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-26 14:20");
+const result3 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-27 15:30");
+const result4 = cachedCalcDateDiff("2023-02-26 14:00", "2023-02-27 15:30");
